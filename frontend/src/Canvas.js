257d3576101fd625b938f107d9c48a7521020ecd
Max <max.nibler@gmail.com>
2020-12-02 13:01:02 -0800

    fake

--- frontend/src/Canvas.js
+++ frontend/src/Canvas.js
@@ -29,7 +29,7 @@ export default function canvas(mailbox) {
   return (
     <div>
       <Typography>
-        {mailbox}
+        mailbox: {mailbox}
       </Typography>
       {mail}
       {getMail(setMail)}
