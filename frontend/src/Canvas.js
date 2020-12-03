69c7df6c103e771d7e28cac1282a2a5bf0f05b1e
Max <max.nibler@gmail.com>
2020-12-03 11:21:38 -0800

    Date format complete

--- frontend/src/Canvas.js
+++ frontend/src/Canvas.js
@@ -62,20 +62,23 @@ function formatDate(time) {
   const inDate = new Date(time);
   let outDate;
   outDate = time;
-  if (date.getMonth() == inDate.getMonth() &&
-    date.getDate() == inDate.getDate()) {
-    let min = inDate.getMinutes();
-    let hour = inDate.getHours();
-    let ampm = ' am';
-    min = ('0'+min).slice(-2);
-    if (hour >= 12) {
-      hour -= 12;
-      ampm = ' pm';
+  if (date.getMonth() == inDate.getMonth()) {
+    if (date.getDate() == inDate.getDate()) {
+      let min = inDate.getMinutes();
+      let hour = inDate.getHours();
+      let ampm = ' am';
+      min = ('0'+min).slice(-2);
+      if (hour >= 12) {
+        hour -= 12;
+        ampm = ' pm';
+      }
+      if (hour == 0) {
+        hour = 12;
+      }
+      outDate = hour+':'+min+ampm;
+    } else if (date.getDate() == inDate.getDate() + 1) {
+      outDate = 'yesterday';
     }
-    if (hour == 0) {
-      hour = 12;
-    }
-    outDate = hour+':'+min+ampm;
   } else if (date > inDate) {
     outDate = months[inDate.getMonth()]+' '+inDate.getDate();
   } else {
