3c4dad79410fcb453bbfcbb93eda0d0b48903854
Max <max.nibler@gmail.com>
2020-12-03 10:17:49 -0800

    recieved mail items and display info

--- frontend/src/Canvas.js
+++ frontend/src/Canvas.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import {List, ListItem} from '@material-ui/core';
+import Divider from '@material-ui/core/Divider';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  list: {
+    width: '100%',
+  },
+  listitem: {
+    width: '100%',
+    border: 1,
+    paddingLeft: theme.spacing(4),
+  },
+}));
 
 /**
  * @param {function} setMail setMail state
+ * @param {string} mailbox
  */
-function getMail(setMail) {
-  fetch('http://172.16.0.18:3010/v0/mailboxes')
+function getMail(setMail, mailbox) {
+  fetch('http://172.16.0.18:3010/v0/mail?mailbox='+mailbox)
       .then((response) => {
         if (!response.ok) {
           throw response;
@@ -13,7 +31,7 @@ function getMail(setMail) {
         return response.json();
       })
       .then((json) => {
-        setMail(json.message);
+        setMail(json);
       })
       .catch((error) => {
         setMail(error.toString());
@@ -21,18 +39,53 @@ function getMail(setMail) {
 }
 
 /**
+ *
+ * @param {obj} email
+ * @return {JSX}
+ */
+function mailItem(email) {
+  return (
+    <div>
+      {email.subject}
+      {email.sent}
+      <Divider/>
+    </div>
+  );
+}
+
+/**
  * @param {string} mailbox
  * @return {JSX}
  */
 export default function canvas(mailbox) {
+  const classes = useStyles();
   const [mail, setMail] = React.useState(undefined);
+
+  const generateMail = () => {
+    const splitMail = mail;
+    splitMail.splice(0, 50);
+    return (
+      <List className={classes.list}>
+        <Divider/>
+        {splitMail.map((item, ind) => (
+          <ListItem
+            key={ind}
+            className={classes.listitem}
+          >
+            {mailItem(item)}
+          </ListItem>
+        ))}
+      </List>
+    );
+  };
+
   return (
     <div>
       <Typography>
         {mailbox}
       </Typography>
-      {mail? mail : 'nothing'}
-      {getMail(setMail)}
+      {getMail(setMail, mailbox)}
+      {mail? generateMail() : 'Error: No mail found'}
     </div>
   );
 }
